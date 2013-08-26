0426ba218040928ba25309fcdef138d23fafd3a1
ja999 <ja999@vp.pl>
2013-08-26 16:44:42 +0200

    Init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MasakraPub::Application.config.secret_token = 'fd27a62c7cf8e633949f72736202e80ce8600d2a9508bbc353c0d5072288709676fb5136542b1019d5cce989e6fb8d4dfeffa514984bc30f5f2bec182f59950c'
